ad25913e4c519dcd897f167bfc695eabb4f31f53
LuisChanocua <esteban083057@gmail.com>
2024-06-22 21:52:40 -0600

    metodos agregar, eliminar, actulizar, obtener empleados, obtener empleado for empleados table

---
+++ CruedReactApiNet8/Controllers/EmpleadoController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using CruedReactApiNet8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CruedReactApiNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly DbacrudReactnetapi8Context dbcontext;
+
+        public EmpleadoController(DbacrudReactnetapi8Context _dbcontext)
+        {
+            dbcontext = _dbcontext;
+        }
+
+        #region GETS
+        //GetEmpleados
+        [HttpGet]
+        [Route("empleados")]
+        public async Task<ActionResult> getEmpleados()
+        {
+            try
+            {
+                var listaEmpleados = await dbcontext.Empleados.ToListAsync();
+                return StatusCode(StatusCodes.Status200OK, listaEmpleados);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                    new
+                    {
+                        message = ex.Message + " No se puedo consultar los empleados",
+                        succes = false,
+                    });
+            }
+        }
+
+        //GetEmpleado
+        [Route("empleado/{idempleado:int}")]
+        public async Task<ActionResult> getEmpleado(int idEmpleado)
+        {
+            try
+            {
+                var empleadoData = await dbcontext.Empleados.FirstOrDefaultAsync(x => x.IdEmpleado == idEmpleado);
+                return StatusCode(StatusCodes.Status200OK, empleadoData);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                    new
+                    {
+                        message = ex.Message + " No se puedo consultar el empleado",
+                        succes = false,
+                    });
+            }
+        }
+        #endregion
+
+
+        #region POST
+        //PostEmpleado: Agregar nuevo usuario
+        [Route("agregarempleado")]
+        public async Task<ActionResult> nuevoEmpleado([FromBody] Empleado empleadoData)
+        {
+
+            try
+            {
+                await dbcontext.Empleados.AddAsync(empleadoData);
+                await dbcontext.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK,
+                    new
+                    {
+                        message = "Empleado agreado",
+                        success = true
+                    });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                    new
+                    {
+                        message = ex.Message + " No se puedo agregar el empleado",
+                        succes = false,
+                    });
+            }
+        }
+        #endregion
+
+        #region PUT
+        //PutEmpleado: Actualizar usuario
+        [Route("editarempleado")]
+        public async Task<ActionResult> actualizarEmpleado([FromBody] Empleado empleadoData)
+        {
+            try
+            {
+                dbcontext.Empleados.Update(empleadoData);
+                await dbcontext.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK,
+                    new
+                    {
+                        message = "Empleado actualizado",
+                        success = true
+                    });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                    new
+                    {
+                        message = ex.Message + " No se puedo actualizar el empleado",
+                        succes = false,
+                    });
+            }
+        }
+        #endregion
+
+        #region DELETE
+        //DeleteEmpleado: Eliminar usuario
+        [Route("deleteempleado/{idempleado:int}")]
+        public async Task<ActionResult> deleteEmpleado(int idEmpleado)
+        {
+            try
+            {
+                var empleado = await dbcontext.Empleados.FirstOrDefaultAsync(x=> x.IdEmpleado == idEmpleado);
+                dbcontext.Empleados.Remove(empleado);
+                await dbcontext.SaveChangesAsync();
+
+                return StatusCode(StatusCodes.Status200OK,
+                                    new
+                                    {
+                                        message = "Empleado eliminado",
+                                        succes = false,
+                                    });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                                    new
+                                    {
+                                        message = ex.Message + " No se puedo eliminar el empleado",
+                                        succes = false,
+                                    });
+            }
+        }
+
+        #endregion
+    }
+}
--- CruedReactApiNet8/Program.cs
+++ CruedReactApiNet8/Program.cs
@@ -1,3 +1,6 @@
+using CruedReactApiNet8.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Bd conection
+builder.Services.AddDbContext<DbacrudReactnetapi8Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
