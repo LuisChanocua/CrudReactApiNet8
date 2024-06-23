da7d7592a8e4a4503e1bf06b847703e5363b8dd8
LuisChanocua <esteban083057@gmail.com>
2024-06-22 20:46:00 -0600

    initial commit

---
+++ CruedReactApiNet8/Models/DbacrudReactnetapi8Context.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CruedReactApiNet8.Models;
+
+public partial class DbacrudReactnetapi8Context : DbContext
+{
+    public DbacrudReactnetapi8Context()
+    {
+    }
+
+    public DbacrudReactnetapi8Context(DbContextOptions<DbacrudReactnetapi8Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=ESTEBANLAPTOPN4;Initial Catalog=dbacrud_reactnetapi8;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__Empleado__CE6D8B9ED424F691");
+
+            entity.ToTable("Empleado");
+
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CruedReactApiNet8/Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CruedReactApiNet8.Models;
+
+public partial class Empleado
+{
+    public int IdEmpleado { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Correo { get; set; }
+
+    public double? Sueldo { get; set; }
+}
